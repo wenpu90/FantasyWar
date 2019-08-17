f43d794a720a1c563296cb82f8318d104272ce8f
wenpu90 <wenpu90@gmail.com>
2019-08-18 07:38:34 +0800

    Battle target selection.

---
+++ Assets/Scripts/BattleSimulation.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class BattleSimulation : MonoBehaviour
+{
+    private static BattleSimulation Instance;
+    public static BattleSimulation GetInstance => Instance;
+
+    public enum GameState { StandbyPhase, DrawCardPhase, PlayerDecisionPhase, PlayerTurnPhase, EnemyTurnPhase }
+    public GameState gameState = GameState.StandbyPhase;
+
+    public List<GameObject> players;
+    public List<GameObject> enemies;
+
+    public delegate void BattleSequence();
+    public List<BattleSequence> BattleSequence_Event;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        //StartCoroutine(AttackMovement());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchGameState(int _state)
+    {
+        gameState = (GameState)_state;
+    }
+
+    public void UseSpecialAttack()
+    {
+        StartCoroutine(AttackMovement());
+    }
+    public void UseItem()
+    {
+        Debug.Log("asd");
+    }
+    public void UseDodge()
+    {
+
+    }
+    public void UseBlock()
+    {
+
+    }
+    public void UseSkill()
+    {
+
+    }
+
+    IEnumerator AttackMovement()
+    {
+        int unit = 0;
+        while(unit < players.Count)
+        {
+            Vector3 vec3 = players[unit].transform.position;
+            players[unit].transform.DOMove(new Vector3(enemies[unit].transform.position.x + 2, 0.5f, enemies[unit].transform.position.z), 0.5f).SetEase(Ease.OutSine);
+            //players[unit].transform.DOJump(new Vector3(enemies[unit].transform.position.x + 1, 0.5f, enemies[unit].transform.position.z), 5, 0, 0.5f, false).SetEase(Ease.InOutQuint);
+            yield return new WaitForSeconds(1);
+            players[unit].transform.DOMove(vec3, 0.5f).SetEase(Ease.OutSine);
+            yield return new WaitForSeconds(2);
+            unit++;
+        }
+
+    }
+}
---
+++ Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    public GameObject target;
+    public enum SelectState {  SelectEnemy, SelectPlayer }
+    public SelectState st = SelectState.SelectEnemy;
+
+    public BattleSimulation bs;
+
+    public int curTargetEnemyNumber = 0;
+    public int curTargetPlayerNumber = 0;
+
+
+    void Start()
+    {
+        bs = BattleSimulation.GetInstance;
+        target.SetActive(true);
+        StartSelectTarget(st == SelectState.SelectEnemy ? true : false);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SwitchEnemyTarget(false, st == SelectState.SelectEnemy ? true : false);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SwitchEnemyTarget(true, st == SelectState.SelectEnemy ? true : false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SelectTarget();
+        }
+    }
+
+    public void StartSelectTarget(bool _PlayerOrEnemy)
+    {
+        SetIndicatorPosition(_PlayerOrEnemy);
+    }
+    
+    public void SwitchEnemyTarget(bool _UpOrDown, bool _PlayerOrEnemy)
+    {
+        switch (_PlayerOrEnemy)
+        {
+            case true:
+                curTargetEnemyNumber = _UpOrDown ? (curTargetEnemyNumber + 1) % (bs.enemies.Count) : curTargetEnemyNumber - 1;
+                if (curTargetEnemyNumber < 0)
+                {
+                    curTargetEnemyNumber = bs.enemies.Count - 1;
+                }
+                break;
+
+            case false:
+                curTargetPlayerNumber = _UpOrDown ? (curTargetPlayerNumber + 1) % (bs.players.Count) : curTargetPlayerNumber - 1;
+                if (curTargetPlayerNumber < 0)
+                {
+                    curTargetPlayerNumber = bs.players.Count - 1;
+                }
+                break;
+        }
+        SetIndicatorPosition(_PlayerOrEnemy);
+
+    }
+    void SetIndicatorPosition(bool _PlayerOrEnemy)
+    {
+        transform.position = _PlayerOrEnemy ? bs.enemies[curTargetEnemyNumber].transform.position : bs.players[curTargetPlayerNumber].transform.position;
+    }
+    void SelectTarget()
+    {
+        float hori;
+        float vert;
+        Vector3 vec3 = new Vector3(hori, 0, vert);
+        transform.Translate(vec3 * speed * Time.deltaTime);
+
+    }
+}

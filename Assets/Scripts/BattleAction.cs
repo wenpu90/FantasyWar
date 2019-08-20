ea5664cbe49b05ada9064330744aa50f3c01e7ec
wenpu90 <wenpu90@gmail.com>
2019-08-20 08:02:28 +0800

    BattleAction

---
+++ Assets/Scripts/BattleAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAction : MonoBehaviour
+{
+    public GameObject[] buttons;
+    public TargetIndicator ti;
+
+    void Start()
+    {
+        ti = TargetIndicator.GetInstance;
+    }
+    public void AttackTarget()//選擇目標
+    {
+
+    }
+    public void SpellTarget()
+    {
+
+    }
+    public void ItemTarget()
+    {
+
+    }
+    public void GuardMode()
+    {
+
+    }
+    public void DodgeMode()
+    {
+
+    }
+}
